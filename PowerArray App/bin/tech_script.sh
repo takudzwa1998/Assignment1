7d6e1674284bf2c260471ab2035b760b1e55fbea
Takudzwa Barnabas Shumbamhini <shmtak004@sl-dual-208.cs.uct.ac.za>
2019-03-03 19:37:17 +0200

    Sunday Changes Part 2

--- PowerArray App/bin/tech_script.sh
+++ PowerArray App/bin/tech_script.sh
@@ -1,12 +1,32 @@
-head -5 cleaned_data.csv>Datasets/Dataset_One.csv
+head -100 cleaned_data.csv>Datasets/Dataset_One.csv
 head -250 cleaned_data.csv>Datasets/Dataset_Two.csv
 head -500 cleaned_data.csv>Datasets/Dataset_Three.csv
 
-while read line
+java DateExtract Datasets/Dataset_One.csv Datasets/DS1.txt
+java DateExtract Datasets/Dataset_Two.csv Datasets/DS2.txt
+java DateExtract Datasets/Dataset_Three.csv Datasets/DS3.txt
+
+
+while read line_ONE
+
 do
 
-set dateTime = $line|cut -d\, -f1
+java PowerBSTApp $line_ONE>>Datasets/BST_Op_Count_Values_BEST_CASE.txt
 
-java PowerArrayApp $line|cut -d\, -f1
+done<Datasets/DS1.txt
 
-done<Datasets/Dataset_One.csv
+while read line_TWO
+
+do
+
+java PowerBSTApp $line_TWO>>Datasets/BST_Op_Count_Values_AVERAGE_CASE.txt
+
+done<Datasets/DS2.txt
+
+while read line_THREE
+
+do
+
+java PowerBSTApp $line_THREE>>Datasets/BST_Op_Count_Values_WORST_CASE.txt
+
+done<Datasets/DS3.txt
